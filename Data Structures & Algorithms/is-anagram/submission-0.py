2ba7ad3b7a0df033fb297068d56f2e3d8cd76b8b
Nishanth <145855771+Nishanth2465@users.noreply.github.com>
2026-09-08 07:46:28 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        d = defaultdict(int) 
+
+        for i in s:
+            d[i] += 1
+        for i in t:
+            d[i] -= 1
+            if d[i] < 0:
+                return False
+
+        return True
+
+        
